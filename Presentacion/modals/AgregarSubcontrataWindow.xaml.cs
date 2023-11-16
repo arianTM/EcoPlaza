eabbee22c7f0525685768f1a63e7fa774f43b0f8
arianTM <88108832+arianTM@users.noreply.github.com>
2023-11-16 16:17:16 -0500

    'AgregarSubcontrataWindow' validation

--- Presentacion/modals/AgregarSubcontrataWindow.xaml.cs
+++ Presentacion/modals/AgregarSubcontrataWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Presentacion.helpers;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Presentacion.modals
 {
@@ -20,14 +10,64 @@ namespace Presentacion.modals
     /// </summary>
     public partial class AgregarSubcontrataWindow : Window
     {
+        #region Constructor
         public AgregarSubcontrataWindow()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region Helpers
+        private bool ValidarCampos()
+        {
+            if (Validador.TextBoxVacio(txtNombre) || Validador.RichTextBoxVacio(txtDescripcion) || Validador.TextBoxVacio(txtRuc) || Validador.TextBoxVacio(txtCelular))
+            {
+                MostrarError("¡Complete los campos!");
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region Errores
+
+        private void OcultarError()
+        {
+            tbError.Text = String.Empty;
+            tbError.Visibility = Visibility.Hidden;
+        }
+
+        private void MostrarError(String mensaje)
+        {
+            tbError.Text = mensaje;
+            tbError.Visibility = Visibility.Visible;
+        }
+        #endregion
+
+        #region Opciones del formulario (Agregar y Limpiar)
+        private void Registrar()
+        {
+            // VALIDAR CAMPOS
+            if (!ValidarCampos()) return;
+
+            // CERRAR FORMULARIO
+            Close();
+        }
 
         private void Limpiar()
         {
+            txtNombre.Clear();
+            txtDescripcion.Document.Blocks.Clear();
+            txtRuc.Clear();
+            txtCelular.Clear();
+        }
+        #endregion
 
+        #region Eventos de Controles
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OcultarError();
         }
 
         private void btnRegresar_Click(object sender, RoutedEventArgs e)
@@ -37,12 +77,33 @@ namespace Presentacion.modals
 
         private void btnAgregar_Click(object sender, RoutedEventArgs e)
         {
-            Close();
+            Registrar();
+        }
+
+        private void txtNombre_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OcultarError();
+        }
+
+        private void txtDescripcion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OcultarError();
+        }
+
+        private void txtRuc_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OcultarError();
+        }
+
+        private void txtCelular_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OcultarError();
         }
 
         private void btnLimpiar_Click(object sender, RoutedEventArgs e)
         {
             Limpiar();
         }
+        #endregion
     }
 }
