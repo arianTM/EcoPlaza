fac06bba12d489856e672af8cc07480bdb853d88
arianTM <88108832+arianTM@users.noreply.github.com>
2023-11-16 01:35:28 -0500

    'AgregarIncidenciaWindow' navigation to 'Incidencia'

--- Presentacion/modals/AgregarIncidenciaWindow.xaml.cs
+++ Presentacion/modals/AgregarIncidenciaWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Presentacion.modals
 {
@@ -22,6 +10,16 @@ namespace Presentacion.modals
         public AgregarIncidenciaWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnRegresar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
