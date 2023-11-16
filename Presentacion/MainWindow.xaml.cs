a4b4466001de26cb5a6ca53664459daf7a80bc1f
arianTM <88108832+arianTM@users.noreply.github.com>
2023-11-16 03:20:06 -0500

    'MainWindow' maximized on startup

--- Presentacion/MainWindow.xaml.cs
+++ Presentacion/MainWindow.xaml.cs
@@ -5,14 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Presentacion
 {
@@ -29,6 +21,8 @@ namespace Presentacion
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             MainFrame.Content = new LoginPage();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            WindowState = WindowState.Maximized;
         }
     }
 }
