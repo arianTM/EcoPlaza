7f4c5536163f18d7a2a9e40743383c17bc8bf2f3
arianTM <88108832+arianTM@users.noreply.github.com>
2023-11-16 16:03:18 -0500

    'AgregarIncidenciaWindow' validation

--- Presentacion/modals/AgregarIncidenciaWindow.xaml.cs
+++ Presentacion/modals/AgregarIncidenciaWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Presentacion.helpers;
+using System;
+using System.Windows;
 
 namespace Presentacion.modals
 {
@@ -7,9 +9,65 @@ namespace Presentacion.modals
     /// </summary>
     public partial class AgregarIncidenciaWindow : Window
     {
+        #region Constructor
         public AgregarIncidenciaWindow()
         {
             InitializeComponent();
+        }
+        #endregion
+
+        #region Helpers
+        private bool ValidarCampos()
+        {
+            if (Validador.RichTextBoxVacio(txtDescripcion) || Validador.ComboBoxSinSeleccion(cbCategoria) || Validador.DatePickerSinSeleccion(dpFecha) || Validador.TimePickerSinSeleccion(tpHora))
+            {
+                MostrarError("¡Complete los campos!");
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region Errores
+
+        private void OcultarError()
+        {
+            tbError.Text = String.Empty;
+            tbError.Visibility = Visibility.Hidden;
+        }
+
+        private void MostrarError(String mensaje)
+        {
+            tbError.Text = mensaje;
+            tbError.Visibility = Visibility.Visible;
+        }
+        #endregion
+
+        #region Opciones del formulario (Agregar y Limpiar)
+        private void Registrar()
+        {
+            // VALIDAR CAMPOS
+            if (!ValidarCampos()) return;
+
+            // CERRAR FORMULARIO
+            Close();
+        }
+
+        private void Limpiar()
+        {
+            txtDescripcion.Document.Blocks.Clear();
+            cbCategoria.SelectedIndex = -1;
+            dpFecha.SelectedDate = null;
+            tpHora.SelectedTime = null;
+        }
+        #endregion
+
+        #region Eventos de Controles
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OcultarError();
         }
 
         private void btnRegresar_Click(object sender, RoutedEventArgs e)
@@ -19,7 +77,34 @@ namespace Presentacion.modals
 
         private void btnAgregar_Click(object sender, RoutedEventArgs e)
         {
-            Close();
+            Registrar();
         }
+
+        private void txtDescripcion_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            OcultarError();
+        }
+
+        private void cbCategoria_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            OcultarError();
+        }
+
+        private void dpFecha_SelectedDateChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            OcultarError();
+        }
+
+        private void tpHora_SelectedTimeChanged(object sender, RoutedPropertyChangedEventArgs<System.DateTime?> e)
+        {
+            OcultarError();
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        #endregion
     }
 }
