e979824e175a8f4d7ce9ee15058f34809ca661cc
arianTM <88108832+arianTM@users.noreply.github.com>
2023-11-16 15:30:12 -0500

    'MaterialesPage' validation

--- Presentacion/pages/MaterialesPage.xaml.cs
+++ Presentacion/pages/MaterialesPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Presentacion.modals;
+﻿using Presentacion.helpers;
+using Presentacion.modals;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,11 +11,17 @@ namespace Presentacion.pages
     /// </summary>
     public partial class MaterialesPage : Page
     {
+        #region Constructor
         public MaterialesPage()
         {
             InitializeComponent();
         }
+        #endregion
 
+        #region Helpers
+        /// <summary>
+        /// Módulos
+        /// </summary>
         private void Navegar(object root)
         {
             // root --> página (new SigninPage(), por ejemplo)
@@ -26,6 +34,68 @@ namespace Presentacion.pages
             window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             window.ShowDialog();
         }
+        private bool DataGridSeleccionado()
+        {
+            if (Validador.DataGridSinSeleccion(dgMateriales))
+            {
+                MostrarError("¡Seleccione un elemento de la tabla de materiales!");
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region Errores
+        private void OcultarError()
+        {
+            tbError.Text = String.Empty;
+            tbError.Visibility = Visibility.Hidden;
+        }
+
+        private void MostrarError(String mensaje)
+        {
+            tbError.Text = mensaje;
+            tbError.Visibility = Visibility.Visible;
+        }
+        #endregion
+
+        #region Opciones de Materiales (Registrar, Modificar y Eliminar)
+        private void Registrar()
+        {
+            OcultarError();
+
+            // ABRIR VENTANA
+            AbrirVentana(new AgregarMaterialWindow());
+        }
+
+        private void Modificar()
+        {
+            OcultarError();
+
+            // VALIDAR SELECCIÓN
+            //if (!DataGridSeleccionado()) return;
+
+            // ABRIR VENTANA
+            AbrirVentana(new ModificarMaterialWindow());
+        }
+
+        private void Eliminar()
+        {
+            OcultarError();
+
+            // VALIDAR SELECCIÓN
+            if (!DataGridSeleccionado()) return;
+        }
+        #endregion
+
+        #region Eventos de Controles
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            OcultarError();
+        }
 
         private void btnRegresar_Click(object sender, RoutedEventArgs e)
         {
@@ -34,12 +104,18 @@ namespace Presentacion.pages
 
         private void btnRegistrar_Click(object sender, RoutedEventArgs e)
         {
-            AbrirVentana(new AgregarMaterialWindow());
+            Registrar();
         }
 
         private void btnModificar_Click(object sender, RoutedEventArgs e)
         {
-            AbrirVentana(new ModificarMaterialWindow());
+            Modificar();
         }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            Eliminar();
+        }
+        #endregion
     }
 }
