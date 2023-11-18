4f02a3af450e646618b2ab2700a449308bb6c161
arianTM <88108832+arianTM@users.noreply.github.com>
2023-11-17 20:05:00 -0500

    'AsistenciasPage' displays asistencias data

--- Presentacion/pages/AsistenciasPage.xaml.cs
+++ Presentacion/pages/AsistenciasPage.xaml.cs
@@ -1,16 +1,38 @@
-﻿using Presentacion.helpers;
+﻿using Datos;
+using Negocio.services;
+using Presentacion.helpers;
 using Presentacion.modals;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace Presentacion.pages
 {
+    #region ViewModel de Asistencias
+    internal class AsistenciaViewModel
+    {
+        public int id { get; set; }
+        public String trabajador { get; set; }
+        public String fecha_hora { get; set; }
+        public String created_by { get; set; }
+        public String created_at { get; set; }
+        public String updated_by { get; set; }
+        public String updated_at { get; set; }
+    }
+    #endregion
     /// <summary>
     /// Interaction logic for AsistenciasPage.xaml
     /// </summary>
     public partial class AsistenciasPage : Page
     {
+        private NAsistencia _nAsistencia = new NAsistencia();
+        private NUsuario _nUsuario = new NUsuario();
+        /// <summary>
+        /// _asistenciasOC guarda los registros de asistencias con el formato de AsistenciaViewModel
+        /// </summary>
+        private ObservableCollection<AsistenciaViewModel> _asistenciasOC { get; set; }
         #region Constructor
         public AsistenciasPage()
         {
@@ -32,7 +54,13 @@ namespace Presentacion.pages
         {
             Window window = (Window)root;
             window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            window.Closed += VentanaCerrada;
             window.ShowDialog();
+        }
+
+        private void VentanaCerrada(object sender, EventArgs e)
+        {
+            MostrarDatos();
         }
 
         private bool DataGridSeleccionado()
@@ -44,6 +72,12 @@ namespace Presentacion.pages
             }
 
             return true;
+        }
+
+        private int ObtenerIdSeleccionado()
+        {
+            AsistenciaViewModel asistenciaSeleccionada = (AsistenciaViewModel)dgAsistencias.SelectedItem;
+            return asistenciaSeleccionada.id;
         }
 
         #endregion
@@ -62,7 +96,47 @@ namespace Presentacion.pages
         }
         #endregion
 
-        #region Opciones de Asistencias (Registrar, Modificar y Eliminar)
+        #region Opciones de Asistencias (Mostrar datos, Registrar, Modificar y Eliminar)
+
+        private void MostrarDatos()
+        {
+            List<Asistencia> asistencias = new List<Asistencia>();
+            try
+            {
+                asistencias = _nAsistencia.GetAsistenciasPorSubcontrata(Administrador.GetIdSubcontrata());
+            }
+            catch (Exception ex)
+            {
+                MostrarError(ex.Message);
+            }
+
+            _asistenciasOC = new ObservableCollection<AsistenciaViewModel>();
+
+            asistencias.ForEach(asistencia =>
+            {
+                DateTime fecha = asistencia.fecha;
+                TimeSpan hora = asistencia.hora;
+                DateTime fechaAsistencia = fecha.Add(hora);
+
+                Usuario creador = _nUsuario.GetUsuario(asistencia.created_by);
+                Usuario editor = _nUsuario.GetUsuario(asistencia.updated_by);
+
+                AsistenciaViewModel materialViewModel = new AsistenciaViewModel()
+                {
+                    id = asistencia.id,
+                    trabajador = asistencia.trabajador,
+                    fecha_hora = fechaAsistencia.ToString("dd/MM/yyyy HH:mm"),
+                    created_by = creador.username,
+                    created_at = asistencia.created_at.ToString("dd/MM/yyyy HH:mm"),
+                    updated_by = editor.username,
+                    updated_at = asistencia.updated_at.ToString("dd/MM/yyyy HH:mm"),
+                };
+
+                _asistenciasOC.Add(materialViewModel);
+            });
+
+            dgAsistencias.ItemsSource = _asistenciasOC;
+        }
 
         private void Registrar()
         {
@@ -98,6 +172,7 @@ namespace Presentacion.pages
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             OcultarError();
+            MostrarDatos();
         }
 
         private void btnRegresar_Click(object sender, RoutedEventArgs e)
