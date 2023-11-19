16656b64fc7a1a80cdd924eafd656cd10024c871
arianTM <88108832+arianTM@users.noreply.github.com>
2023-11-18 22:19:19 -0500

    Dashboard reports structure implemented

--- Presentacion/pages/MenuPage.xaml.cs
+++ Presentacion/pages/MenuPage.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Datos;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
 using Negocio.services;
 using Presentacion.helpers;
 using Presentacion.modals;
@@ -6,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Presentacion.pages
 {
@@ -23,6 +27,9 @@ namespace Presentacion.pages
             InitializeComponent();
             MostrarDatosUsuario();
             MostrarSubcontratas();
+            MostrarReportes();
+
+            DataContext = this;
         }
         #endregion
 
@@ -89,6 +96,162 @@ namespace Presentacion.pages
 
             // NAVEGAR
             Navegar(new SubcontrataPage());
+        }
+
+        #endregion
+
+        #region Dashboard (reportes)
+
+        #region Reporte 1
+
+        public SeriesCollection R1SeriesCollection { get; set; }
+
+        private void MostrarReporte1()
+        {
+            R1SeriesCollection = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title = "Chrome",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(8) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Mozilla",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(6) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Opera",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(10) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Explorer",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(4) },
+                    DataLabels = true
+                }
+            };
+        }
+
+        #endregion
+
+        #region Reporte 2
+        private void MostrarReporte2()
+        {
+
+        }
+        #endregion
+
+        #region Reporte 3
+
+        public SeriesCollection R3SeriesCollection { get; set; }
+        public string[] R3Labels { get; set; }
+        public Func<double, string> R3YFormatter { get; set; }
+
+        private void MostrarReporte3()
+        {
+            R3SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }
+            };
+
+            R3Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            R3YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            R3SeriesCollection.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+                PointGeometrySize = 50,
+                PointForeground = Brushes.Gray
+            });
+
+            //modifying any series values will also animate and update the chart
+            R3SeriesCollection[3].Values.Add(5d);
+        }
+        #endregion
+
+        #region Reporte 4
+        private void MostrarReporte4()
+        {
+
+        }
+        #endregion
+
+        #region Reporte 5
+        public SeriesCollection R5SeriesCollection { get; set; }
+
+        private void MostrarReporte5()
+        {
+            R5SeriesCollection = new SeriesCollection
+            {
+                new PieSeries
+                {
+                    Title = "Chrome",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(8) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Mozilla",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(6) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Opera",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(10) },
+                    DataLabels = true
+                },
+                new PieSeries
+                {
+                    Title = "Explorer",
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(4) },
+                    DataLabels = true
+                }
+            };
+        }
+        #endregion
+
+        #region Reporte 6
+        private void MostrarReporte6()
+        {
+
+        }
+        #endregion
+
+        private void MostrarReportes()
+        {
+            MostrarReporte1();
+            MostrarReporte2();
+            MostrarReporte3();
+            MostrarReporte4();
+            MostrarReporte5();
+            MostrarReporte6();
         }
 
         #endregion
