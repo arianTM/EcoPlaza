b500f76074c3fdb96c9c7fd3ff09f79fc4863370
arianTM <88108832+arianTM@users.noreply.github.com>
2023-11-18 23:59:16 -0500

    'Proveedor' class created in Negocio

---
+++ Negocio/Proveedor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Negocio
+{
+    /// <summary>
+    /// Clase que provee entidades en código (que no se deben almacenar como base de datos)
+    /// </summary>
+    public static class Proveedor
+    {
+        public static List<String> GetMarcas()
+        {
+            return new List<String>()
+            {
+                "DeWalt",
+                "Makita",
+                "Bosch",
+                "Milwaukee",
+                "Hilti",
+                "Stanley",
+                "Metabo HPT",
+                "Caterpillar",
+                "Ryobi",
+            };
+        }
+    }
+}
